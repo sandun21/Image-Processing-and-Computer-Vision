583a0e891fc1cb1370481cf1a46ca8497cb57a35
Herath HMSI <herathsandun21@gmail.com>
2024-09-25 18:31:18 +0530

    generate initial blocks for other parts

---
+++ Intensity Transformations and Neighborhood Filtering/codes/q5.py
@@ -0,0 +1,6 @@
+import cv2                  
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread("../images/shells.tif")         # As applying gamma correction on L plane
+L, a, b = cv2.split(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))     
