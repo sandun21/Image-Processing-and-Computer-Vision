01e4cf2f6c210ca8e73e7b433f1b0772a44cf190
Herath HMSI <herathsandun21@gmail.com>
2024-09-25 22:13:13 +0530

    q5 code done(implementation of historgram equalization)

--- Intensity Transformations and Neighborhood Filtering/codes/q5.py
+++ Intensity Transformations and Neighborhood Filtering/codes/q5.py
@@ -2,5 +2,20 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread("../images/shells.tif")         # As applying gamma correction on L plane
-L, a, b = cv2.split(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))     
+image = cv2.imread("../images/shells.tif")     
+
+#find frequency of occurance of each pixel value in 2D array without using historgram function 
+unique, counts = np.unique(image, return_counts=True)
+frequency = dict(zip(unique, counts))
+numpx = np.prod(image.shape)
+multfactor = 255/numpx
+hist = np.array([frequency[i] if i in frequency.keys() else 0 for i in range(256)])
+
+print("earlier historgram", hist)
+cdf = np.cumsum(hist)
+t = np.floor(cdf * multfactor).astype(np.uint8)
+newim = t[image]
+
+print("new historgram", t)
+cv2.imshow("Original Image", newim)
+cv2.waitKey(0)  
