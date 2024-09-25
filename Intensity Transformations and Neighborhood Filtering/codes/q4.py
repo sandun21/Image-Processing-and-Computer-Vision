ddf85a23d470901782e3f7a91cf1b227f511a146
Herath HMSI <herathsandun21@gmail.com>
2024-09-25 19:42:04 +0530

    question 4 completed(visually pleasing a value havent found)

--- Intensity Transformations and Neighborhood Filtering/codes/q4.py
+++ Intensity Transformations and Neighborhood Filtering/codes/q4.py
@@ -2,5 +2,21 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread("../images/spider.png")         # As applying gamma correction on L plane
-L, a, b = cv2.split(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))     
+image = cv2.imread("../images/spider.png")         
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)    
+h, s, v = cv2.split(hsv)                          # Split image in to hue, saturation and value
+
+a = 0.9
+sigma = 70
+x = np.arange(0, 256)
+t = x + a * 128 * np.exp(-((x - 128) ** 2) / (2 * sigma ** 2))
+t = np.clip(t, 0, 255)  
+Snew = cv2.LUT(s, t).astype(np.uint8)
+new_img = cv2.merge([h, Snew, v])
+new_img = cv2.cvtColor(new_img, cv2.COLOR_HSV2BGR)
+
+
+cv2.imshow("Original Image", image)
+cv2.imshow("Corrected Image", new_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
