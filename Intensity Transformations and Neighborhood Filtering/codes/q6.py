75a65ce9a2a4e47f0cbdf7b254de0c9a6db17950
Herath HMSI <herathsandun21@gmail.com>
2024-09-26 19:58:00 +0530

    extract layers in grayscale for image

--- Intensity Transformations and Neighborhood Filtering/codes/q6.py
+++ Intensity Transformations and Neighborhood Filtering/codes/q6.py
@@ -2,5 +2,23 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread("../images/jeniffer.jpg")         # As applying gamma correction on L plane
-L, a, b = cv2.split(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))     
+image = cv2.imread("../images/jeniffer.jpg")        # As applying gamma correction on L plane
+assert image is not None
+h, s, v = cv2.split(cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL))
+
+# Show 3 channels in grayscale in matplotlib plots
+plt.figure(figsize=(15, 5))
+plt.subplot(131)
+plt.imshow(h, cmap='gray')
+plt.title('Hue')
+plt.subplot(132)
+plt.imshow(s, cmap='gray')
+plt.title('Saturation')
+plt.subplot(133)
+plt.imshow(v, cmap='gray')
+plt.title('Value')
+plt.show()
+
+
+#saturation plane is better to extract foreground mask
+# obtain foreground using cv2.bitwise_and
