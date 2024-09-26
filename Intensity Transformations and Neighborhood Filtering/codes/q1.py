e508cecf5c8d0ccb4c008d33a8d57fb25f50b82e
Herath HMSI <herathsandun21@gmail.com>
2024-09-26 21:53:05 +0530

    make historgram equalization code in q5 functional

--- Intensity Transformations and Neighborhood Filtering/codes/q1.py
+++ Intensity Transformations and Neighborhood Filtering/codes/q1.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 f = cv2.imread("../images/emma.jpg", cv2.IMREAD_GRAYSCALE)   # Read image in grayscale
+assert f is not None
 
 arr1 = np.linspace(0,50,50,dtype='uint8')
 arr2 = np.linspace(100,255,100,dtype='uint8')
@@ -9,9 +10,13 @@ arr3 = np.linspace(150,255,106,dtype='uint8')
 
 # Lookup table for the transformation
 t = np.concatenate((arr1, arr2, arr3),dtype='uint8')        # Concatenate arrays
-
 g = cv2.LUT(f, t)                                           # Apply intensity transformation
+
+#save two images
+cv2.imwrite("outputs/q1emma_transformed.jpg", g)
+cv2.imwrite("outputs/q1emma.jpg", f)
 
 cv2.imshow("Original Image", f)
 cv2.imshow("Transformed Image", g)
 cv2.waitKey(0)
+
--- Intensity Transformations and Neighborhood Filtering/codes/q5.py
+++ Intensity Transformations and Neighborhood Filtering/codes/q5.py
@@ -3,19 +3,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 image = cv2.imread("../images/shells.tif")     
+assert image is not None
 
-#find frequency of occurance of each pixel value in 2D array without using historgram function 
-unique, counts = np.unique(image, return_counts=True)
-frequency = dict(zip(unique, counts))
-numpx = np.prod(image.shape)
-multfactor = 255/numpx
-hist = np.array([frequency[i] if i in frequency.keys() else 0 for i in range(256)])
+def historgram(image):
+    '''
+       function that takes image as the input and returns the frequency of occurance of each pixel value
+    '''
+    unique, counts = np.unique(image, return_counts=True)
+    frequency = dict(zip(unique, counts))
+    hist = np.array([frequency[i] if i in frequency.keys() else 0 for i in range(256)])
+    return hist
 
-print("earlier historgram", hist)
-cdf = np.cumsum(hist)
-t = np.floor(cdf * multfactor).astype(np.uint8)
-newim = t[image]
+def histEqualization(image):
+    '''
+       function that takes image to be equalized as the input and returns the equalized image 
+    '''
+    numpx = np.prod(image.shape)    
+    multfactor = 255/numpx
+    cdf = np.cumsum(historgram(image))
+    
+    t = np.floor(cdf * multfactor).astype(np.uint8)                
+    newimg = t[image]
+    return newimg
 
-print("new historgram", t)
-cv2.imshow("Original Image", newim)
+
+newim = histEqualization(image)
+
+#historgrams before and after normalization
+plt.figure(figsize=(10, 5))
+plt.subplot(121)
+plt.hist(image.ravel(), 256, [0, 256])
+plt.title('Original Image')
+plt.xlabel('Intensity')
+plt.ylabel('Frequency')
+plt.subplot(122)
+plt.hist(newim.ravel(), 256, [0, 256])
+plt.title('Equalized Image')
+plt.xlabel('Intensity')
+plt.ylabel('Frequency')
+plt.show()
+
+cv2.imshow("Equilized Image", newim)
 cv2.waitKey(0)  
