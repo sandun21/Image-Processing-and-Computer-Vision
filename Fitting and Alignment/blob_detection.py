eddbd7fce1119c4f94fa1b2280cbf86f3e173563
Herath HMSI <herathsandun21@gmail.com>
2024-10-22 17:16:47 +0530

    detected blobs for single variance

--- Fitting and Alignment/blob_detection.py
+++ Fitting and Alignment/blob_detection.py
@@ -7,10 +7,26 @@ f = cv2.imread(r"images\\the_berry_farms_sunflower_field.jpeg", cv2.IMREAD_REDUC
 assert f is not None
 gray = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY).astype(np.float32) / 255.0   # Normalized grayscale image
 
-sigma = 4.0
+sigma = 8
 log_image = gaussian_laplace(gray, sigma=sigma)
+log_image = (sigma ** 2) * np.abs(log_image)
 
-plt.imshow(log_image, cmap='gray')
-plt.title('Laplacian of Gaussian')
-plt.axis('off')
+threshold = 0.30
+neighborhood_size = 10
+
+# Detect local maxima
+local_max = maximum_filter(log_image, size=neighborhood_size) == log_image
+blobs = (log_image > threshold) & local_max
+
+# Get coordinates of blobs
+y, x = np.nonzero(blobs)
+r = np.sqrt(2) * sigma
+
+# Plot blobs
+fig, ax = plt.subplots()
+ax.imshow(gray, cmap='gray')
+for y_i, x_i in zip(y, x):
+    c = plt.Circle((x_i, y_i), r, color='red', linewidth=0.5, fill=False)
+    ax.add_patch(c)
+ax.set_axis_off()
 plt.show()
